a306083b258fb6258c9552699a311f51a3b81996
Ben Ramseth <bramseth@esri.com>
2014-10-08 21:51:21 -0400

    moveJS out of html file

--- js/map.js
+++ js/map.js
@@ -1,3 +1,26 @@
 /**
  * Created by BennyBaruba on 10/8/2014.
  */
+var map;
+require(["esri/map",
+        "esri/layers/ArcGISDynamicMapServiceLayer",
+        "dojo/domReady!"],
+    function(Map,
+             ArcGISDynamicMapServiceLayer) {
+
+        //Turn of detection for CORS
+        esriConfig.defaults.io.corsDetection = false;
+
+        map = new Map("map", {
+            basemap: "streets",
+            center: [-76.09, 36.78], // longitude, latitude
+            zoom: 11
+        });
+
+        var dynamicMapServiceLayer = new ArcGISDynamicMapServiceLayer("http://maps.vbgov.com/ArcGISimap/rest/services/Administrative_Boundaries/MapServer", {
+            "opacity" : 1.0
+        });
+
+        map.addLayer(dynamicMapServiceLayer);
+
+    });
