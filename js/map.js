87342972c776352fe671d51e3e3ac0e53ced28c4
Ben Ramseth <bramseth@esri.com>
2014-10-10 16:58:31 -0400

    TOC not working, but scaffolded and basemap widget also there, but dysfunctional

--- js/map.js
+++ js/map.js
@@ -1,49 +1,143 @@
 /**
  * Created by BennyBaruba on 10/8/2014.
  */
+var djConfig = {
+    parseOnLoad: true,
+    baseUrl: './',
+    modulePaths: {
+        'agsjs': location.pathname.replace(/\/[^/]+$/, '') + './agsjs/TOC.js'
+    }
+};
+
 var map;
-require(["esri/map",
-        "esri/layers/ArcGISDynamicMapServiceLayer",
-        "dojo/domReady!"],
-    function(Map,
-             ArcGISDynamicMapServiceLayer) {
+
+require([
+   //Map and Layers
+    "esri/map",
+    "esri/layers/ArcGISDynamicMapServiceLayer",
+   //Map dijits
+    "esri/dijit/OverviewMap",
+    "esri/dijit/HomeButton",
+    "esri/dijit/LocateButton",
+    "esri/dijit/Scalebar",
+    "esri/dijit/BasemapGallery",
+    //???
+    "esri/arcgis/utils",
+    //Layout dijits
+    "dijit/layout/BorderContainer",
+    "dijit/layout/ContentPane",
+    "dijit/TitlePane",
+
+    //TOC
+    "agsjs/dijit/TOC",
+
+    "dojo/parser",
+    "dojo/domReady!"],
+
+    function(
+        Map,
+        ArcGISDynamicMapServiceLayer,
+
+        OverviewMap,
+        HomeButton,
+        LocateButton,
+        Scalebar,
+        BasemapGallery,
+        arcgisUtils,
+
+        BorderContainer,
+        ContentPane,
+        TitlePane,
+
+        TOC,
+
+        parser
+        ){
 
         //Turn of detection for CORS
         esriConfig.defaults.io.corsDetection = false;
 
+        //parser.parse();
         map = new Map("map", {
             basemap: "streets",
             center: [-76.09, 36.78], // longitude, latitude
             zoom: 11
         });
 
-        var dynamicMapServiceLayer = new ArcGISDynamicMapServiceLayer("http://maps.vbgov.com/ArcGISimap/rest/services/Administrative_Boundaries/MapServer", {
+        //Add Administrative_Boundaries as a dynamic layer
+        var Administrative_Boundaries = new ArcGISDynamicMapServiceLayer("http://maps.vbgov.com/ArcGISimap/rest/services/Administrative_Boundaries/MapServer", {
             "opacity" : 1.0
         });
+        map.addLayer(Administrative_Boundaries);
+        Administrative_Boundaries.visible=true;
 
-        map.addLayer(dynamicMapServiceLayer);
-
-        var dynamicMapServiceLayer2 = new ArcGISDynamicMapServiceLayer("http://maps.vbgov.com/ArcGISimap/rest/services/Administrative_Boundaries/MapServer", {
+        //Add points_of_interest as a dynamic layer
+        var points_of_interest  = new ArcGISDynamicMapServiceLayer("http://maps.vbgov.com/ArcGISimap/rest/services/PointsOfInterest_VBgov/MapServer", {
             "opacity" : 1.0
         });
+        map.addLayer(points_of_interest);
+        points_of_interest.visible=true;
 
-        map.addLayer(dynamicMapServiceLayer2);
-
-        var dynamicMapServiceLayer3 = new ArcGISDynamicMapServiceLayer("http://maps.vbgov.com/ArcGISimap/rest/services/Administrative_Boundaries/MapServer", {
+        //Add property_information as a dynamic layer
+        var property_information = new ArcGISDynamicMapServiceLayer("http://maps.vbgov.com/ArcGISimap/rest/services/PropertyInformation_VBgov/MapServer", {
             "opacity" : 1.0
         });
+        map.addLayer(property_information);
+        property_information.visible=false;
 
-        map.addLayer(dynamicMapServiceLayer3);
-
-        var dynamicMapServiceLayer4 = new ArcGISDynamicMapServiceLayer("http://maps.vbgov.com/ArcGISimap/rest/services/Administrative_Boundaries/MapServer", {
+        //Add emergency_preparedness as a dynamic layer
+        var emergency_preparedness = new ArcGISDynamicMapServiceLayer("http://maps.vbgov.com/ArcGISimap/rest/services/Emergency_Preparedness/MapServer", {
             "opacity" : 1.0
         });
+        map.addLayer(emergency_preparedness);
+        emergency_preparedness.visible=false;
 
-        map.addLayer(dynamicMapServiceLayer4);
-
-        var dynamicMapServiceLayer5 = new ArcGISDynamicMapServiceLayer("http://maps.vbgov.com/ArcGISimap/rest/services/Administrative_Boundaries/MapServer", {
+        //Add flood_zones_preliminary as a dynamic layer
+        var flood_zones_preliminary = new ArcGISDynamicMapServiceLayer("http://maps.vbgov.com/ArcGISimap/rest/services/Flood_Zones_Preliminary/MapServer", {
             "opacity" : 1.0
         });
+        map.addLayer(flood_zones_preliminary);
+        flood_zones_preliminary.visible=false;
 
-        map.addLayer(dynamicMapServiceLayer5);
+        //Dijits that connect to the map
+        //Overview Map
+        var overviewMapDijit = new OverviewMap({
+            map: map,
+            visible: true
+        });
+        overviewMapDijit.startup();
+
+        //Home Button
+        var home = new HomeButton({
+            map: map
+        }, "HomeButton");
+        home.startup();
+
+        //LocateButton
+        geoLocate = new LocateButton({
+            map: map
+        }, "LocateButton");
+        geoLocate.startup();
+
+        //ScaleBar
+        var scalebar = new Scalebar({
+            map: map,
+            // "dual" displays both miles and kilmometers
+            // "english" is the default, which displays miles
+            // use "metric" for kilometers
+            scalebarUnit: "dual"
+        });
+
+        //BaseMapGallery
+        var basemapGallery = new BasemapGallery({
+            showArcGISBasemaps: true,
+            map: map
+        }, "basemapGallery");
+        basemapGallery.startup();
+
+        basemapGallery.on("error", function(msg) {
+            console.log("basemap gallery error:  ", msg);
+        });
+
+
     });
